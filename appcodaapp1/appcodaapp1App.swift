69dd0cc1c694344c3f0822840cd1ee2861a7fb88
mohammadaboelnasr <m.aboelnasr@live.com>
2025-07-29 18:25:56 +0300

    Initial Commit

---
+++ appcodaapp1/appcodaapp1App.swift
@@ -0,0 +1,17 @@
+//
+//  appcodaapp1App.swift
+//  appcodaapp1
+//
+//  Created by Mohammad Aboelnasr on 29/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct appcodaapp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
