d408b67d90f267dabd62c890c62c9fc33185b68b
mohammadaboelnasr <m.aboelnasr@live.com>
2025-07-30 23:15:45 +0300

    till chapter 8

---
+++ appcodaapp1/RestaurantListView.swift
@@ -0,0 +1,51 @@
+//
+//  ListAndForEach.swift
+//  appcodaapp1
+//
+//  Created by Mohammad Aboelnasr on 30/07/2025.
+//
+
+import SwiftUI
+
+struct RestaurantListView: View {
+    let restaurantNames: [String] = [
+        "Cafe Deadend",
+        "Homei",
+        "Teakha",
+        "Cafe Loisl",
+        "Petite Oyster",
+        "For Kee Restaurant",
+        "Po's Atelier",
+        "Bourke Street Bakery",
+        "Haigh's Chocolate",
+        "Palomino Espresso",
+        "Upstate",
+        "Traif",
+        "Graham Avenue Meats And Deli",
+        "Waffle & Wolf",
+        "Five Leaves",
+        "Cafe Lore",
+        "Confessional",
+        "Barrafina",
+        "Donostia",
+        "Royal Oak",
+        "CASK Pub and Kitchen"
+    ]
+    var body: some View {
+        List{
+            ForEach (restaurantNames,id: \.self){ restaurantName in
+                HStack {
+                    Image("\(restaurantName)")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                    Text(restaurantName)
+                }
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+#Preview {
+    RestaurantListView()
+}
---
+++ appcodaapp1/Student.swift
@@ -0,0 +1,18 @@
+//
+//  Student.swift
+//  appcodaapp1
+//
+//  Created by Mohammad Aboelnasr on 29/07/2025.
+//
+
+import SwiftUI
+
+struct Student: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Student()
+}
